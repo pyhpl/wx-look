836ff70973f716ae40d3632912be9fb3f1f953f8
jingling.long <https://mail.aorise.org/#5>
2018-03-17 23:12:17 +0800

    add a little activity content

--- pages/activity/activity.js
+++ pages/activity/activity.js
@@ -1,5 +1,20 @@
 
 Page({
-  data: {
+  data: {    
+    tags: [
+      { id: 1, name: "推荐" },
+      { id: 2, name: "电影" },
+      { id: 3, name: "电影" },
+      { id: 4, name: "电影" },
+      { id: 5, name: "电影" },
+      { id: 6, name: "电影" },
+      { id: 7, name: "电影" }
+    ],
+    currentId: 1
+  },
+  navbarTagTaped: function (e) {
+    this.setData({
+      currentId: e.target.dataset.id
+    });
   }
 })
