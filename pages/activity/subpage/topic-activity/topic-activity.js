2140c90e8a8711e21af14d875b8762877c1b9adb
jingling.long <https://mail.aorise.org/#5>
2018-04-04 22:12:26 +0800

    topic-activity add activity-content (just save)

--- pages/activity/subpage/topic-activity/topic-activity.js
+++ pages/activity/subpage/topic-activity/topic-activity.js
@@ -3,7 +3,7 @@ import util from "../../../../utils/util.js";
 
 grace.page({
   data: {
-    // 主题相关数据
+    /***** 主题相关数据 ****/
     topic: {
       pictureUrl: 'https://images-1252933270.cos.ap-guangzhou.myqcloud.com/zuozhu.jpg',
       title: "秋池渐涨，秋叶渐黄",
@@ -12,6 +12,75 @@ grace.page({
     },
     more: false, // 是否显示更多
     less: false, // 是否显示更少
+    /***** 活动相关数据 ****/
+    isShowNewest: true, // 是否显示最新
+    isShowHotest: false, // 是否显示最热
+    // 活动数据
+    activitys: [
+      {
+        avatarUrl: "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/zuozhu.jpg",
+        initiator: "随风",
+        school: "云南大学",
+        title: "世人谓我恋长安，其实只恋长安某",
+        descPictureUrls: [
+          "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/hahaha.jpg",
+          "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/hahaha.jpg"
+        ],
+        tag: "火影忍者",
+        joinPeople: 1234,
+        likeCount: 1234,
+        publishDate: "2018-03-18"
+      },
+      {
+        avatarUrl: "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/zuozhu.jpg",
+        initiator: "随风",
+        school: "云南大学",
+        title: "世人谓我恋长安，其实只恋长安某",
+        descPictureUrls: [
+          "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/hahaha.jpg",
+          "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/hahaha.jpg",
+          "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/hahaha.jpg",
+          "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/hahaha.jpg",
+          "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/hahaha.jpg",
+          "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/hahaha.jpg",
+          "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/hahaha.jpg",
+        ],
+        tag: "火影忍者",
+        joinPeople: 1234,
+        likeCount: 1234,
+        publishDate: "2018-03-18"
+      },
+      {
+        avatarUrl: "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/zuozhu.jpg",
+        initiator: "随风",
+        school: "云南大学",
+        title: "世人谓我恋长安，其实只恋长安某",
+        descPictureUrls: [
+          "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/hahaha.jpg",
+          "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/hahaha.jpg",
+          "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/hahaha.jpg",
+          "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/hahaha.jpg",
+        ],
+        tag: "火影忍者",
+        joinPeople: 1234,
+        likeCount: 1234,
+        publishDate: "2018-03-18"
+      },
+      {
+        avatarUrl: "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/zuozhu.jpg",
+        initiator: "随风",
+        school: "云南大学",
+        title: "世人谓我恋长安，其实只恋长安某",
+        descPictureUrls: [
+          "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/hahaha.jpg",
+          "https://images-1252933270.cos.ap-guangzhou.myqcloud.com/hahaha.jpg"
+        ],
+        tag: "火影忍者",
+        joinPeople: 1234,
+        likeCount: 1234,
+        publishDate: "2018-03-18"
+      }
+    ]
   },
   // ************************* 生命周期方法 ************************* //
   onLoad() {
@@ -42,5 +111,23 @@ grace.page({
       current: e.currentTarget.dataset.src, // 当前显示图片的http链接
       urls: [e.currentTarget.dataset.src] // 需要预览的图片http链接列表
     })
+  },
+  // 预览图片
+  imagePreview: function (e) {
+    var imgList = e.currentTarget.dataset.list == undefined ? [e.currentTarget.dataset.src] : e.currentTarget.dataset.list; //获取data-list
+    wx.previewImage({
+      current: e.currentTarget.dataset.src, // 当前显示图片的http链接
+      urls: imgList // 需要预览的图片http链接列表
+    })
+  },
+  // 
+  chooseNav: function(e) {
+    if (e.currentTarget.dataset.nav == 'new') {
+      this.$data.isShowNewest = true;
+      this.$data.isShowHotest = false;
+    } else if (e.currentTarget.dataset.nav == 'hot') {
+      this.$data.isShowHotest = true;
+      this.$data.isShowNewest = false;
+    }
   }
 })
